cba2bbec1029df51668ba7e4499dea79ba191102
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-06-24 13:20:23 -0400

    modified  Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,50 @@
-/**
- * Created by iyasuwatts on 10/17/17.
+import java.util.Random;
+
+/*
+  Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
+
 public class Main {
 
-    public static void main(String[] args){
-        
+
+
+    private static void guessGame() {
+        // Random
+        Random rand = new Random();
+        int guess = rand.nextInt(100);
+        int guessCounter = 0;
+        int guessMachine = 0;
+
+        // Scanner
+        Scanner input = new Scanner(System.in);
+
+
+        // while loop
+        while (guess != 0) {
+            System.out.println("Please enter a number");
+            int playerInput = input.nextInt();
+            guessCounter++;
+            if (playerInput < guess) {
+                System.out.println("Too low ");
+            } else if (playerInput > guess) {
+                System.out.println("Too high");
+            } else {
+                System.out.println("You Win!" + " Tries: " + guessCounter);
+                guessMachine++;
+                break;
+            }
+        }
     }
+
+    public static void main(String[] args) {
+        // calling guessGame
+        guessGame();
+    }
+
+
 }
+
+
