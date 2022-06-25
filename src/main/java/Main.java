30591463ab13e0f0f2e662850a78277951eb3b8d
Fitru <fitru@ZipcodersMBP100.fios-router.home>
2022-06-24 22:14:14 -0400

    Added a way to reduce guessCounter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,45 +6,51 @@ import java.util.Random;
 
 import java.util.Scanner;
 
-
 public class Main {
-
-
 
     private static void guessGame() {
         // Random
         Random rand = new Random();
         int guess = rand.nextInt(100);
         int guessCounter = 0;
-        int guessMachine = 0;
 
         // Scanner
         Scanner input = new Scanner(System.in);
 
-
         // while loop
+        int playerInputHolder = 0;
+        int holder = 0;
+
         while (guess != 0) {
             System.out.println("Please enter a number");
             int playerInput = input.nextInt();
             guessCounter++;
+            playerInputHolder = playerInput;
+
+            if(playerInput == holder) {
+                guessCounter--;
+            }
+                if(playerInputHolder!=0){
+                holder=playerInputHolder;
+            }
+
             if (playerInput < guess) {
                 System.out.println("Too low ");
             } else if (playerInput > guess) {
                 System.out.println("Too high");
             } else {
                 System.out.println("You Win!" + " Tries: " + guessCounter);
-                guessMachine++;
                 break;
             }
-        }
+            System.out.println(guessCounter);
+            }
     }
 
     public static void main(String[] args) {
+
         // calling guessGame
         guessGame();
     }
-
-
 }
 
 
